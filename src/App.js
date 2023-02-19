c6b0867122249752a7049bd2d14fe78b4529245e
bobshort4bobby4 <treborken@gmail.com>
2023-02-19 15:41:52 +0000

    finished memory game project in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard  from './components/SingleCard';
+
+const cardImages = [
+    { "src": "/img/helmet-1.png", matched: false },
+    { "src": "/img/potion-1.png", matched: false },
+    { "src": "/img/ring-1.png", matched: false },
+    { "src": "/img/scroll-1.png", matched: false },
+    { "src": "/img/shield-1.png", matched: false },
+    { "src": "/img/sword-1.png", matched: false },
+  ]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+
+// shuffle cards
+const shuffleCards = () => {
+  const shuffledCards = [...cardImages, ...cardImages]
+  .sort(() => Math.random() -.5 )
+  .map((card) => ({...card, id: Math.random()}))
+  setCards(shuffledCards)
+  setTurns(0)
+  
+}
+
+// handle choice
+
+const handleChoice = (card) => {
+  console.log(card)
+  choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
+  // not here
+  
+}
+
+// compare two cards
+
+useEffect(()  =>{
+  
+  if (choiceOne && choiceTwo) {
+    setDisabled(true)
+    if ( choiceOne.src === choiceTwo.src){
+      setCards(prevCards => {
+        return prevCards.map(card =>{
+          if(card.src === choiceOne.src) {
+            return {...card, matched: true}
+          } else {
+            return card
+          }
+        })
+      })
+      resetTurns()
+    } else {
+      console.log("unmatched")
+      setTimeout(() => resetTurns(), 1000)
+      
+    }
+
+    
+  }
+
+}, [choiceOne, choiceTwo])
+console.log(cards)
+
+// reset choices and increase turns
+
+const resetTurns = () => {
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1 )
+  setDisabled(false)
+}
+// start a new game automatically
+
+useEffect(() => shuffleCards(), [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard  
+          key={card.id} 
+          card= {card}
+          handleChoice={handleChoice}
+          flipped={card=== choiceOne || card=== choiceTwo || card.matched}
+          disabled={disabled}
+
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
